23f538bffe55d7c760467731b6a4cf0f591060e3
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-12-08 21:35:15 -0300

    Iniciando projeto

---
+++ api-cadastro-clientes/src/main/java/com/cadastro/clientes/controller/ClienteController.java
@@ -0,0 +1,11 @@
+package com.cadastro.clientes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	
+}
